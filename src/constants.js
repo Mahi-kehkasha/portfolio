191b4521785ba7dfdb49cb22e410f8c8bd1c8740
mahi-kehkasha <connectmaheenk@gmail.com>
2024-11-12 21:22:30 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,104 @@
+export const techStack = [
+  {
+    name: 'C Language',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg',
+  },
+
+  {
+    name: 'Python',
+    link: 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png',
+  },
+
+  {
+    name: 'Machine Learning Algorithmns',
+    link: 'https://png.pngtree.com/png-vector/20230407/ourmid/pngtree-deep-learning-line-icon-vector-png-image_6688986.png',
+  },
+  {
+    name: 'Postman',
+    link: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
+  },
+
+  {
+    name: 'React Native',
+    link: 'https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png',
+  },
+  {
+    name: 'HTML',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  },
+  {
+    name: 'CSS',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
+  },
+  {
+    name: 'Bootstrap',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg',
+  },
+  {
+    name: 'Redux',
+    link: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg',
+  },
+
+  {
+    name: 'Javascript',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    name: 'Tailwind CSS',
+    link: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
+  },
+  {
+    name: 'tableau',
+    link: 'https://e7.pngegg.com/pngimages/875/824/png-clipart-tableau-software-computer-software-business-intelligence-data-visualization-tableau-software-computer-software.png',
+  },
+  {
+    name: 'MySQl',
+    link: 'https://w7.pngwing.com/pngs/154/976/png-transparent-database-computer-icons-sql-others-angle-data-analytics-thumbnail.png',
+  },
+  {
+    name: 'Mongo DB',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg',
+  },
+
+  {
+    name: 'Git',
+    link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg',
+  },
+];
+
+export const contactLinks = [
+  {
+    name: 'Github',
+    url: 'https://img.icons8.com/doodle/40/000000/github--v1.png',
+    link: 'https://github.com/Mahi-kehkasha',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png',
+    link: 'https://www.linkedin.com/in/maheen-kehkasha-68b658224/',
+  },
+];
+
+export const serviceData = [
+  {
+    name: 'Web Application Development',
+    desc: 'Web design encompasses many different skills and disciplines in the production and maintenance of websites. The different areas of web design include web graphic design user interface design  authoring, including standardised code and proprietary software user experience design and search engine',
+    img: 'https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png',
+  },
+  {
+    name: 'Data analyst',
+    desc: `We provide comprehensive data analysis services, including data collection, cleaning, and visualization. 
+    Our expertise spans statistical analysis, predictive modeling, and business intelligence, enabling data-driven decision-making.
+     We integrate data from diverse sources to uncover insights, optimize performance, and drive strategic growth`,
+    img: 'https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png',
+  },
+
+  {
+    name: 'Backend Development',
+    desc: `Backend development involves building and maintaining the server, application, and database that power the frontend. By focusing on 
+    logic, security, and scalability, backend developers ensure that applications perform efficiently and handle data seamlessly.
+     This includes developing APIs, managing databases, 
+    and integrating systems to create a solid foundation that supports the frontend and enhances the user experience`,
+    img: 'https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png',
+  },
+];
